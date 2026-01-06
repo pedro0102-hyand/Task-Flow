dc9287759a3de26f122d099823a8bd2477f0482d
sucupirap <pedro41julio@gmail.com>
2026-01-06 20:56:32 -0300

    dicionario

---
+++ structures/task_registry.py
@@ -0,0 +1,17 @@
+class TaskRegistry:
+
+    def __init__(self):
+
+        self.tasks = {}
+
+    def add(self, task):
+
+        self.tasks[task.id] = task
+    
+    def get(self, task_id):
+
+        return self.tasks.get(task_id)
+    
+    def all_task(self):
+
+        return list(self.tasks.values())
