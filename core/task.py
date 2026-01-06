c9c7d36124f1c9f95710dc7c29103c49ead5ebe5
sucupirap <pedro41julio@gmail.com>
2026-01-06 20:11:45 -0300

    comments

--- core/task.py
+++ core/task.py
@@ -4,10 +4,10 @@ class Task:
 
     def __init__(self, task_id, title, priority):
 
-        self.id = id
+        self.id = id # chave no dicionario
         self.title = title
         self.priority = priority
-        self.status = TaskStatus.BACKLOG
+        self.status = TaskStatus.BACKLOG # status inicial no backlog
         self.dependencies = set()
 
     def __repr__(self):
